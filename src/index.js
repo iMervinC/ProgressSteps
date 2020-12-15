520dd3bddd4644e901b0b4591eef0a6fb33e3fd0
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2020-12-15 11:02:40 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import "./styles.css";
+
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circle = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+
+  if (currentActive > circle.length) {
+    currentActive = circle.length;
+  }
+
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circle.forEach((x, inx) => {
+    if (inx < currentActive) {
+      x.classList.add("active");
+    } else {
+      x.classList.remove("active");
+    }
+  });
+
+  const active = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((active.length - 1) / (circle.length - 1)) * 100 + "%";
+}
